c63ba2746a99b4e02c4e809ad74c068098ca5a18
edouardm <edouard.maisonneuve@gmail.com>
2025-02-24 15:01:59 +0100

    preliminary implementation of the tests done

--- src/ScoreboardLib/api/Scoreboard.java
+++ src/ScoreboardLib/api/Scoreboard.java
@@ -13,4 +13,20 @@ public class Scoreboard {
         listMatches = new HashMap<Integer, Match>();
     }
 
+    public Integer startMatch(String nameHomeTeam, String nameVisitorTeam) {
+        return 0;
+    }
+
+    public void updateScore(Integer matchId, Integer scoreHomeTeam, Integer scoreVisitorTeam) {
+
+    }
+
+    public void terminateMatch(Integer matchId) {
+
+    }
+
+    public String getMatchesSummary() {
+        return "";
+    }
+
 }
--- tests/TestScoreboard.java
+++ tests/TestScoreboard.java
@@ -1,8 +1,33 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import ScoreboardLib.api.Scoreboard;
+import ScoreboardLib.internal.Match;
 
 class TestScoreboard {
+
+    static public Map<Integer,Match> helperGetListMatchesForBoard(Scoreboard board) {
+
+        Map<Integer,Match> retrievedMatches = null;
+
+        try {
+            // Access private fields via reflection for testing purposes
+            Field matches = Scoreboard.class.getDeclaredField("listMatches");
+            matches.setAccessible(true);
+            retrievedMatches = (Map<Integer,Match>) matches.get(board);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return retrievedMatches;
+    }
 
     /**
     *
@@ -13,7 +38,16 @@ class TestScoreboard {
     
         @Test
         void BoardCreated_ExpectsEmptyListMatches() {
-            fail("Test not implemented yet");
+
+            Scoreboard board = new Scoreboard();
+
+            Map<Integer,Match> retrievedMatches;
+
+            assertNotNull(board, "The Scoreboard object should not be null");
+			retrievedMatches = TestScoreboard.helperGetListMatchesForBoard(board);
+
+            assertTrue(retrievedMatches.keySet().isEmpty(),
+                "The newly created Scoreboard contains some entries while it should not");
         }    
     }
 
@@ -26,17 +60,54 @@ class TestScoreboard {
     
         @Test
         void ValidTeamNames_MatchCreatedWithNullScore() {
-            fail("Test not implemented yet");
+
+            String nameTeam1 = "France";
+            String nameTeam2 = "Argentina";
+            String nameTeam3 = "Spain";
+            String nameTeam4 = "England";
+
+            Map<Integer,Match> listMatches;
+            Integer match1Id;
+            Integer match2Id;
+
+            Scoreboard board = new Scoreboard();
+
+            match1Id = board.startMatch(nameTeam1, nameTeam2);
+
+            listMatches = TestScoreboard.helperGetListMatchesForBoard(board);
+            assertTrue(1 == listMatches.size(),"Match1 was not successfully added");
+
+            match2Id = board.startMatch(nameTeam3, nameTeam4);
+            assertTrue(2 == listMatches.size(),"Match2 was not successfully added");
+            assertTrue(match1Id != match2Id,"Match1 and Match2 have the same unique Id");
+        }
+
+        @Test
+        void NullTeamNames_MatchNotCreatedAndExceptionThrown() {
+            
+            Scoreboard board = new Scoreboard();
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+				board.startMatch(null,null);
+	        });
+	        assertEquals("At least 1 null team name passed", exception.getMessage());
         }
 
         @Test
         void EmptyTeamNames_MatchNotCreatedAndExceptionThrown() {
-            fail("Test not implemented yet");
+            Scoreboard board = new Scoreboard();
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+				board.startMatch("","     ");
+	        });
+	        assertEquals("At least 1 empty team name passed", exception.getMessage());
         }
 
         @Test
         void SameTeamNames_MatchNotCreatedAndExceptionThrown() {
-            fail("Test not implemented yet");
+            Scoreboard board = new Scoreboard();
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+				board.startMatch("France","France");
+	        });
+	        assertEquals("Invalid team names (the same for both teams)", exception.getMessage());
         }
     }
 
@@ -44,42 +115,155 @@ class TestScoreboard {
     class UpdateScoreTests {
     
         @Test
+        @Disabled("not testable here, will be tested while generating matches summary")
         void ValidScoreProvidedForValidMatchId_ScoreUpdated() {
-            fail("Test not implemented yet");
+            
         }
 
         @Test
         void InvalidMatchId_ExceptionThrown() {
-            fail("Test not implemented yet");
+            Scoreboard board = new Scoreboard();
+
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+				board.updateScore(45,2,2);;
+	        });
+	        assertEquals("Error: trying to update an invalid match", exception.getMessage());
         }
 
         @Test
         void NegativeScores_ExceptionThrown() {
-            fail("Test not implemented yet");
+            Scoreboard board = new Scoreboard();
+
+            Integer matchId = board.startMatch("France", "Argentina");
+
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+				board.updateScore(matchId,-2,-2);;
+	        });
+	        assertEquals("Error: trying to provide a negative score", exception.getMessage());
         }
     }
 
-        @Nested
+    @Nested
     class GetMatchesSummaryTests {
     
         @Test
         void NoMatchTracked_EmptyString() {
-            fail("Test not implemented yet");
+            Scoreboard board = new Scoreboard();
+            String summary = board.getMatchesSummary();
+
+            assertTrue(0 == summary.compareTo(""),
+                "The summary is supposed to empty when no match is being tracked");
         }
 
         @Test
         void DifferentScores_MatchesSortedPerNumberGoals() {
-            fail("Test not implemented yet");
+            String name1 = "England";
+            String name2 = "Spain";
+            String name3 = "France";
+            String name4 = "Argentina";
+
+            Integer scoreFrance = 4;
+            Integer scoreArgentina = 2;
+            Integer scoreEngland = 1;
+            Integer scoreSpain = 2;
+
+            Integer EngEspMatchId;
+            Integer FraArgMatchId;
+
+            String matchesSummary;
+            
+            String expectedResult = name3 + " " + scoreFrance.toString() + " - " +
+                    name4 + " " + scoreArgentina.toString() + "\n" + 
+                    name1 + " " + scoreEngland.toString() + " - " + 
+                    name2 + " " + scoreSpain.toString();
+
+            Scoreboard board = new Scoreboard();
+            EngEspMatchId = board.startMatch(name1, name2);
+            FraArgMatchId = board.startMatch(name3, name4);
+
+            board.updateScore(EngEspMatchId, scoreEngland, scoreSpain);
+            board.updateScore(FraArgMatchId, scoreFrance, scoreArgentina);
+
+            matchesSummary = board.getMatchesSummary();
+
+            assertTrue(0 == matchesSummary.compareTo(expectedResult), "Incorrect matches summary");
         }
 
         @Test
         void SameScores_MatchesSortedPerCreationDate() {
-            fail("Test not implemented yet");
+            String name1 = "England";
+            String name2 = "Spain";
+            String name3 = "France";
+            String name4 = "Argentina";
+
+            Integer scoreFrance = 1;
+            Integer scoreArgentina = 2;
+            Integer scoreEngland = 1;
+            Integer scoreSpain = 2;
+
+            Integer EngEspMatchId;
+            Integer FraArgMatchId;
+
+            String matchesSummary;
+            
+            String expectedResult = name1 + " " + scoreEngland.toString() + " - " +
+                    name2 + " " + scoreSpain.toString() + "\n" + 
+                    name3 + " " + scoreFrance.toString() + " - " + 
+                    name4 + " " + scoreArgentina.toString();
+
+            Scoreboard board = new Scoreboard();
+            EngEspMatchId = board.startMatch(name1, name2);
+            FraArgMatchId = board.startMatch(name3, name4);
+
+            board.updateScore(EngEspMatchId, scoreEngland, scoreSpain);
+            board.updateScore(FraArgMatchId, scoreFrance, scoreArgentina);
+
+            matchesSummary = board.getMatchesSummary();
+
+            assertTrue(0 == matchesSummary.compareTo(expectedResult), "Incorrect matches summary");
         }
 
         @Test
         void CombinationOfCases_MatchesProperlySorted() {
-            fail("Test not implemented yet");
+            String england = "England";
+            String spain = "Spain";
+            String france = "France";
+            String argentina = "Argentina";
+            String germany = "Germany";
+            String brazil = "Brazil";
+
+            Integer scoreFrance = 1;
+            Integer scoreArgentina = 2;
+            Integer scoreEngland = 1;
+            Integer scoreSpain = 2;
+            Integer scoreGermany = 5;
+            Integer scoreBrazil = 2;
+
+            Integer EngEspMatchId;
+            Integer FraArgMatchId;
+            Integer GerBraMatchId;
+
+            String matchesSummary;
+            
+            String expectedResult = germany + " " + scoreGermany.toString() + " - " +
+                    brazil + " " + scoreBrazil.toString() + "\n" + 
+                    england + " " + scoreEngland.toString() + " - " +
+                    spain + " " + scoreSpain.toString() + "\n" + 
+                    france + " " + scoreFrance.toString() + " - " + 
+                    argentina + " " + scoreArgentina.toString();
+
+            Scoreboard board = new Scoreboard();
+            EngEspMatchId = board.startMatch(england, spain);
+            FraArgMatchId = board.startMatch(france, argentina);
+            GerBraMatchId = board.startMatch(germany, brazil);
+
+            board.updateScore(EngEspMatchId, scoreEngland, scoreSpain);
+            board.updateScore(FraArgMatchId, scoreFrance, scoreArgentina);
+            board.updateScore(GerBraMatchId, scoreGermany, scoreBrazil);
+
+            matchesSummary = board.getMatchesSummary();
+
+            assertTrue(0 == matchesSummary.compareTo(expectedResult), "Incorrect matches summary");
         }
     }
 }
